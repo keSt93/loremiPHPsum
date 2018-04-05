2e0f17f208b5d23fc0f6ceb2b1dd84941fd49c6e
keSt <kevin.stein93@gmail.com>
2018-04-05 14:45:31 +0200

    page shouldnt reload anymore on generation, prevented memory crash

--- index.php
+++ index.php
@@ -7,16 +7,19 @@
 	<?php
 		$loremText = "";
 		$loremIpsumFileContents = file_get_contents("lorem.txt");
-  
-		// check if our form has been submitted...
-		if(isset($_POST['submit'])){
-			
-			// read form values and act accordingly
+  		
+		if(array_key_exists('submit',$_POST)){
+		   	// read form values and act accordingly
 			$loremType=$_POST['lorem_type'];
 			$loremValue=$_POST['lorem_value'];
 			
+			
 			// if user wants letters, loop lorem text as often as needed...			
 			if($loremType=="letters") {
+				// fallback to lower value, because memory 
+				if($loremValue > 99999) {
+					$loremValue = 99999;
+				}
 				if ($loremValue > 9384) {
 					$loremRepeat = ceil($loremValue / 9384);
 					for($i = 1; $i <= $loremRepeat; $i++) {
@@ -32,6 +35,10 @@
 				
 			// if user wants words, loop lorem text as often as needed
 			} else if($loremType=="words") {
+				// fallback to lower value, because memory 
+				if($loremValue > 9999) {
+					$loremValue = 9999;
+				}
 				if($loremValue > 1374) {
 					$loremRepeat = ceil($loremValue / 1374);
 					for($i = 1; $i <= $loremRepeat; $i++) {
@@ -62,7 +69,7 @@
 	?>
 	
 	<!-- form -->
-	<form id="lorem_settings" action ="" method="post">
+	<form id="lorem_settings" method="post">
 		<input type="text" name="lorem_value" id="lorem_words" value="<?php echo $loremValue ?>" maxlength="10" size="6"/>
 		<select name="lorem_type" id="lorem_type">
 			<option value="words" <?php if($loremWordsSelected) { echo 'selected'; } ?>> Wörter </option>
